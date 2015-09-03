44942e835bafbcee7e2ad25e070e337b56b61df7
Dane Regnier <dane.regnier@gmail.com>
2015-09-03 15:03:59 -0500

    Initial Commit

---
+++ Nerdfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nerdfeed
+//
+//  Created by Dane on 9/3/15.
+//  Copyright (c) 2015 Regnier Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
