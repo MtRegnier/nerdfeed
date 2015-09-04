d1b2d5306f02a33faf627ccac426ef061a7f65e3
Dane Regnier <dane.regnier@gmail.com>
2015-09-03 19:08:32 -0500

    Finished Silver Challenge

--- Nerdfeed/BNRCoursesViewController.m
+++ Nerdfeed/BNRCoursesViewController.m
@@ -75,7 +75,7 @@
 
 - (void)fetchFeed
 {
-    NSString *requestString = @"https://bookapi.bignerdranch.com/private/courses.json";
+    NSString *requestString = @"http://bookapi.bignerdranch.com/courses.json";
     NSURL *url = [NSURL URLWithString:requestString];
     NSURLRequest *req = [NSURLRequest requestWithURL:url];
     
--- Nerdfeed/BNRWebViewController.m
+++ Nerdfeed/BNRWebViewController.m
@@ -8,13 +8,35 @@
 
 #import "BNRWebViewController.h"
 
+@interface BNRWebViewController ()
+
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) UIToolbar *toolbar;
+@property (nonatomic, strong) UIBarButtonItem *forwardButton;
+@property (nonatomic, strong) UIBarButtonItem *backButton;
+
+@end
+
 @implementation BNRWebViewController
 
 - (void)loadView
 {
-    UIWebView *webView = [[UIWebView alloc] init];
-    webView.scalesPageToFit = YES;
-    self.view = webView;
+    self.webView = [[UIWebView alloc] init];
+    self.webView.scalesPageToFit = YES;
+    CGRect toolbarFrame = CGRectMake(0, 984, [[UIScreen mainScreen]bounds].size.width, 40);
+    self.toolbar = [[UIToolbar alloc] initWithFrame:toolbarFrame];
+    self.backButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" 
+                                                                   style:UIBarButtonItemStylePlain
+                                                                  target:self
+                                                                  action:@selector(goBackPage)];
+    self.forwardButton = [[UIBarButtonItem alloc] initWithTitle:@"Forward"
+                                                                      style:UIBarButtonItemStylePlain
+                                                                     target:self
+                                                                     action:@selector(goForwardPage)];
+    self.toolbar.items = @[self.backButton, self.forwardButton];
+    
+    [self.webView addSubview:self.toolbar];
+    self.view = self.webView;
 }
 
 - (void)setURL:(NSURL *)URL
@@ -24,6 +46,26 @@
         NSURLRequest *req = [NSURLRequest requestWithURL:_URL];
         [(UIWebView *)self.view loadRequest:req];
     }
+}
+
+#pragma mark Toolbar Button Setup
+
+- (void)goBackPage
+{
+    if (!self.webView.canGoBack) {
+        return;
+    }
+    
+    [self.webView goBack];
+}
+
+- (void)goForwardPage
+{
+    if (!self.webView.canGoForward) {
+        return;
+    }
+    
+    [self.webView goForward];
 }
 
 @end
